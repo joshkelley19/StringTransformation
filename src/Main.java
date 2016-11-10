ae434ead1379c3c39046f5c240616386f31a6f6c
joshuakelley <joshkelley19@gmail.com>
2016-11-10 07:35:53 -0500

    updated version

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,21 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Main {
-    public static Map<Integer, String> map = new LinkedHashMap<>();
-    static final String[] onesTeens = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-    static final String[] tens = {"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
-    static final String[] denominations = {"Hundred","Thousand","Million","Billion",""};
+    private static Map<Integer, String> map = new LinkedHashMap<>();
+    private static final String[] onesTeens = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",
+            "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    private static final String[] tens = {"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    private static final String[] denominations = {"Hundred","Thousand","Million","Billion","Dollars"};
+
     public static void main(String[] args) {
         System.out.println(stringTransformation("123456789"));
     }
 
     public static String stringTransformation(String s){
         if(s.length()>9)return "OneBillionDollars";
-        int number = Integer.parseInt(s,10);
         int ind;
+        int number = Integer.parseInt(s,10);
+        if(number<1)return "ZeroDollars";
         StringBuilder sb = new StringBuilder();
         fillMap();
         if(number>999999){
@@ -23,19 +26,14 @@ public class Main {
         if(number>999){
             ind = ((number%1000000)-(number%1000))/1000;
             sb.append(build(ind,1));
-
         }
-        if(number>0){
-            ind = number%1000;
-            sb.append(build(ind,4));
-        }
-        sb.append("Dollars");
+        ind = number%1000;
+        sb.append(build(ind,4));
         return sb.toString();
     }
 
-    static String build(int number,int dem){
-        String num = String.format("%03d",number);
-        System.out.println(num);
+    private static String build(int digits,int denomination){
+        String num = String.format("%03d",digits);
         StringBuilder sb = new StringBuilder();
         int h, t, o;
         h = Integer.parseInt(num.substring(0,1));
@@ -43,13 +41,13 @@ public class Main {
         o = Integer.parseInt(num.substring(2,3));
         if(h!=0)sb.append(map.get(h)).append(denominations[0]);
         if(t!=0){
-            if(t==0){
-                sb.append(map.get(Integer.parseInt(num.substring(1,3)))).append(dem);
+            if(t==1){
+                sb.append(map.get(Integer.parseInt(num.substring(1,3)))).append(denominations[denomination]);
                 return sb.toString();
             }else{sb.append(map.get(t*10));}
         }
         if(o!=0)sb.append(map.get(o));
-        sb.append(denominations[dem]);
+        sb.append(denominations[denomination]);
         return sb.toString();
     }
 
